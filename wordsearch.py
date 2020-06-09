8d69cc2f30cc5a338789bf43030482ec24894484
Caden Claussen <caden.claussen@gmail.com>
2020-06-08 19:32:10 -0700

    finished validate

--- wordsearch.py
+++ wordsearch.py
@@ -3,6 +3,7 @@ import random
 boardSize = 10
 board = []
 words = ["cat", "dog"]
+directions = ["right"]
 
 
 def main():
@@ -10,7 +11,7 @@ def main():
     printBoard()
     insertWords()
     printBoard()
-    replaceDot()
+#    replaceDot()
     printBoard()
     
 
@@ -29,14 +30,35 @@ def printBoard():
         print()
     print()
     print()
-
+    
 def insertWords():
     for word in words:
-        row = random.randint(0, boardSize - 1)
-        column = random.randint(0, boardSize - 1)
-        for ch in word:
-            board[row][column] = ch
-            column = column + 1    
+        while True:
+            row = random.randint(0, boardSize - 1)
+            column = random.randint(0, boardSize - 1)
+            direction = random.choice(directions)        
+            valid = isValid(word, row, column, direction)
+            if valid == False:
+                continue
+            insertWord(word, row, column, direction)
+            break
+
+def isValid(word, row, column, direction):
+    for ch in word:
+        if column >= boardSize:
+            return False
+        if row >= boardSize:
+            return False
+        if board[row][column] != ".":
+            if board[row][column] != ch:
+                return False
+        column = column + 1
+    return True
+
+def insertWord(word, row, column, direction):
+    for ch in word:
+        board[row][column] = ch.upper()
+        column = column + 1    
 
 def replaceDot():
     letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -45,8 +67,6 @@ def replaceDot():
             letterPut = random.choice(letters)
             if board[row][column] == ".":
                 board[row][column] = letterPut
-                
-
 
 main()
-
+        
