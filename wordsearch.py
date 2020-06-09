fe46cf3be581901d4cdb715bfe58ae1fd3a8b272
Shane Claussen <sclaussen@yahoo.com>
2020-06-09 12:29:57 -0700

    Spell checked words, and added a few

--- wordsearch.py
+++ wordsearch.py
@@ -5,31 +5,13 @@ boardSize = 30
 board = []
 
 
-# test
-#words = ["cat", "dog"]
-
-# humanities
-#words = [ "humanities", "humans", "ancestor", "cultures", "civilizations", "angelisland", "ellisisland", "stereotypes", "nativeamericans", "ohlone", "artifact", "immigration", "goldrush", "migration", "generation", "chinese", "mexican", "bracero", "depression", "segregation", "slavery", "civilrights" ]
-
-# math
-# words = ["math", "addition", "subtraction", "multiplication", "division", "negitive", "powers", "squareroots", "factors", "multiples", "prime", "primefactors", "primefactorazation", "lcm", "gcf", "patterns", "systematic", "operation", "numbers", "digits", "doubel", "triple", "single", "quadruple"]
-
-# science
-# words = ["chemist", "science", "experement", "mixture", "plant", "bird", "flight", "stem", "steam", "recation", "molecule", "adom", "neculer", "power", "mass", "light", "sound", "water", "waves, "tension", "refraction", "defraction", "garden"]
-
-# sel
+words = [ "humanities", "humans", "ancestor", "cultures", "civilizations", "angelisland", "ellisisland", "stereotype", "nativeamerican", "ohlone", "artifact", "immigration", "goldrush", "migration", "generation", "chinese", "mexican", "bracero", "depression", "segregation", "slavery", "civilrights", "phoenixes" ]
+# words = [ "math", "addition", "subtraction", "multiplication", "division", "negative", "powers", "squareroots", "factors", "multiples", "prime", "primefactors", "primefactorization", "lcm", "gcf", "patterns", "systematic", "operation", "numbers", "digits", "double", "triple", "single", "quadruple" ]
+# words = [ "science", "chemist", "experiment", "mixture", "plant", "bird", "flight", "stem", "steam", "reaction", "molecule", "atom", "nuclear", "power", "mass", "light", "sound", "water", "waves", "tension", "refraction", "defraction" ]
 # words = ["sel", "helpfull", "imessege", "trashcan", "cumunity", "exclusion", "inclusion", "feelings", "emotion", "emotionthermomometer", "lisen", "heart", "calm", "tone", "selfreflection", "talkitout", "intension", "ideas", "mind", "eyes", "descolate"]
-
-# swi
 # words = ["noun", "verb", "adverb", "adjective", "handwriting", "story", "base", "sufix", "compoundword", "swi", "prefix", "root", "greek", "latin", "middleinglish", "oldinglish", "final", "meadel", "inital", "vowel"]
-
-# pe
 # words = ["exersize", "running", "harder", "healthy", "diet", "food", "jumpropes", "sports", "football", "basketball", "socer", "hokey", "climbing", "skiing", "swiming", "mucels", "bones", "body", "pingpong", "tinnes"]
-
-# head
-# words = ["cumunity", "forts", "mansion", "teachers", "colaberation", "leaders", "helpers", "leadership", "campus", "grationsness", "spirt", "kind", "thankfull", "nueva", "care", "idea", "exclusion", "humor", "wisdom", "ilab", "sel", "swi", "maverics"]
-
-# laraine
+# words = [ "nueva", "community", "forts", "mansion", "teachers", "collaboration", "leadership", "campus", "gracious", "spirit", "kind", "thankful", "caring", "innovate", "inclusion", "humor", "wisdom", "ilab", "elab", "sel", "swi", "quest", "stemfair", "steam", "ilc", "garden", "trips", "auction", "klawn", "mavericks", "balletlawn" ]
 # words = ["kindergarden", "gingerbreadman", "investagation", "magnifiengglass", "detective", "klawn", "choice", "mysteryreader", "circletime", "devenchi", "monalisa", "postoffice", "sandbox", "garden", "teacher", "joeyarea", "classroom", "snacktime", "morningmeeting", "scedwal", "readingtime", "clues", "curiosity"]
 
 
@@ -51,23 +33,23 @@ def initilizeBoard():
         for _ in range(boardSize):
             board[row].append('.')
 
+
 def printBoard():
-    print()
-    print()
-    print("    ", end="")
-    for column in range(boardSize):
-        print(column % 10, end="   ")
     print()
     print()
     for row in range(boardSize):
         for column in range(boardSize):
             if (column == 0):
-                print(row % 10, end="   ")
-            print(board[row][column], end="   ")
+                print("    ", end="")
+            if (board[row][column] == '.'):
+                print(" ", end="   ")
+            else:
+                print(board[row][column], end="   ")
         print()
         print()
     print()
     print()
+
 
 def insertWords():
     for word in words:
@@ -80,6 +62,7 @@ def insertWords():
                 continue
             insertWord(word, row, column, direction)
             break
+
 
 def isValid(word, row, column, direction):
     for ch in word:
@@ -102,12 +85,14 @@ def isValid(word, row, column, direction):
 
     return True
 
+
 def insertWord(word, row, column, direction):
     for ch in word:
         board[row][column] = ch
         updateDirectionList = updateDirection(row, column, direction)
         row = updateDirectionList[0]
         column = updateDirectionList[1]
+
 
 def replaceDot():
     letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -116,6 +101,7 @@ def replaceDot():
             letterPut = random.choice(letters)
             if board[row][column] == ".":
                 board[row][column] = letterPut
+
 
 def updateDirection(row, column, direction):
     if direction == "right":
